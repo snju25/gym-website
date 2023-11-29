054fdcece62c421ec057d1d96ff6fd6a2eb97619
Sanjay Bhandari <bhandarisanjay879@gmail.com>
2023-11-29 15:35:59 +1000

    first commit

---
+++ components/footer.js
@@ -0,0 +1,35 @@
+document.write(`
+<section class="footer-content">
+<section>
+    <h2>Locations</h2>
+    <ul class="footer-ul">
+        <li>Ashgrove</li>
+        <li>Brisbane City</li>
+        <li>Chermside</li>
+        <li>Graceville</li>
+        <li>Westlake</li>
+    </ul>
+</section>
+<section>
+    <h2>Train With Us </h2>
+    <ul class="footer-ul">
+        <li>Membership</li>
+        <li>Classes</li>
+        <li>Contact us</li>
+        <li>About us</li>
+    </ul>
+</section>
+<section>
+    <h2>Social </h2>
+    <ul class="footer-ul">
+        <li>Facebook</li>
+        <li>Instagram</li>
+        <li>Twitter</li>
+    </ul>
+</section>
+</section>
+<section class="copyright">
+<span>© 2023 MLStrength. All rights reserved. Terms and ConditionsPrivacy Policy</span>
+</section>
+
+`)
---
+++ components/menu.js
@@ -0,0 +1,50 @@
+document.write(`<header id="mobile-view">
+    <a href="/index.html"><img src="../images/logo.png" class="logo" alt="Logo of ML Strength"></a>
+    <label for="burger-menu-toggle" id="burger-menu-toggle-button"><img src="/images/menu.svg" alt="Hamburger Menu for drop down navigation menu"></label>
+
+</header>
+    <section id="burger-menu-overlay">
+        <input type="checkbox" name="burger-menu" id="burger-menu-toggle">
+        <nav id="burger-menu">
+            <a href="/index.html" class="menu-items">
+                Home
+            </a>
+            <a href="/html/contactus.html" class="menu-items">
+                Contact Us
+            </a>
+            <a href="/html/classes.html" class="menu-items">
+                Classes
+            </a>
+            <a href="/html/membership.html" class="menu-items">
+                Membership
+            </a>
+            <a href="/html/aboutus.html" class="menu-items">
+                About Us
+            </a>
+        </nav>
+    </section>
+
+<header id="wide-view">
+    <nav class="menu menu-top">
+        <a href="/index.html"><img src="../images/logo.png" class="logo" alt="Logo of ML Strength"></a>
+        <section class="header_menu">
+            <a href="/index.html" class="menu-items">
+                Home
+            </a>
+            <a href="/html/contactus.html" class="menu-items">
+                Contact Us
+            </a>
+            <a href="/html/classes.html" class="menu-items">
+                Classes
+            </a>
+            <a href="/html/membership.html" class="menu-items">
+                Membership
+            </a>
+            <a href="/html/aboutus.html" class="menu-items">
+                About Us
+            </a>
+        </section>
+        <a href="/html/membership.html" class="btn">Join now</a>
+    </nav>
+</header>
+`)
